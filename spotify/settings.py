2b9d0104fc5d6847c703eeba18e27ccfc2361130
ThiagoDiasV <thiago76ers@gmail.com>
2020-07-10 19:38:30 -0300

    deploy heroku

--- spotify/settings.py
+++ spotify/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY", default="weaksecretkey")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost", cast=Csv())
 
